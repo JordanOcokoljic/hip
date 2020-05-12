07597de1d47befee80d2af925c3592a9360e289b
Jordan Ocokoljic <jordan@libreware.org>
2020-05-12 12:12:19 +1000

    Initial commit

---
+++ main.nim
@@ -0,0 +1,18 @@
+# This file is part of Nip, a tool for manaing Typescript React projects and
+# components.
+# Copyright (C) 2020 Jordan Ocokoljic.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+echo "Hello, world!"
